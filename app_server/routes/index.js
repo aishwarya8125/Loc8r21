f213091d840a94fa25ee43ea3da9454f22564c08
aishwarya8125 <21eg106a49@anurag.edu.in>
2023-08-10 11:52:21 +0530

    First Commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var ctrlLocations=require('../controller/locations')
+var ctrlOthers=require('../controller/others')
+
+router.get('/',ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location2',ctrlLocations.locationInfo2);
+router.get('/location3',ctrlLocations.locationInfo3);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+/*var homepagecontroller=function(req,res)
+ {
+  res.render('index', { title: "Mean class of AI-A" })
+};*/
+router.get('/about',ctrlOthers.about);
+module.exports = router;
